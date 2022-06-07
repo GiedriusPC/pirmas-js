2464541ec2843b1fbbd2246eeb82c358eade5aca
Giedrius <messagesbox@outlook.com>
2022-06-07 14:13:19 +0300

    monetu formule

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const username2 = 'Jonas';
 
 const username3 = 'Antanas';
 
-let pinigine1 = 3;
+let pinigine1 = 2;
 
-let pinigine2 = 7;
+let pinigine2 = 4;
 
-let pinigine3 = 9;
+let pinigine3 = 6;
 
-console.log (username1, 'pinigineje turi', pinigine1, 'monetas');
-console.log (username2, 'pinigineje turi', pinigine2, 'monetas');
-console.log (username3, 'pinigineje turi', pinigine3, 'monetas');
+console.log (username1, 'pinigineje turi', pinigine1, 'monetas.');
+console.log (username2, 'pinigineje turi', pinigine2, 'monetas.');
+console.log (username3, 'pinigineje turi', pinigine3, 'monetas.');
 
-console.log (username1, 'ir', username3, 'atidave savo monetas Jonui', 'todel', username2, 'dabar turi', pinigine1 + pinigine3 + pinigine2)
+console.log (username1, 'ir', username3, 'atidave savo monetas Jonui, todel', username2, 'dabar turi', pinigine1 + pinigine3 + pinigine2, 'monetu.');
+console.log ('Veliau', username2, 'padalino monetas visiems po lygiai, todel dabar visi turi po', 12 / 3, 'monetas');
