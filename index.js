902d8060d1b153491c7c7c9abd45a75efdda2d74
Giedrius <messagesbox@outlook.com>
2022-06-07 14:18:24 +0300

    monetu dalybu formule

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ console.log (username1, 'pinigineje turi', pinigine1, 'monetas.');
 console.log (username2, 'pinigineje turi', pinigine2, 'monetas.');
 console.log (username3, 'pinigineje turi', pinigine3, 'monetas.');
 
-console.log (username1, 'ir', username3, 'atidave savo monetas Jonui, todel', username2, 'dabar turi', pinigine1 + pinigine3 + pinigine2, 'monetu.');
-console.log ('Veliau', username2, 'padalino monetas visiems po lygiai, todel dabar visi turi po', 12 / 3, 'monetas');
+console.log (username1, 'ir', username3, 'atidave savo monetas Jonui, todel', username2, 'dabar turi', pinigine = (pinigine1 + pinigine2 + pinigine3), 'monetu.');
+console.log ('Veliau', username2, 'padalino monetas visiems po lygiai, todel dabar visi turi po', pinigine / 3, 'monetas');
